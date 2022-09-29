af578ea9b323cb5999bd1f4144217189a0c99276
Yuliia Hrabovenko <yuliia.hrabovenko@gmail.com>
2022-09-29 15:41:04 +0300

    homework9: Constructor and method clear() modification

--- src/main/java/homework9/MyArrayList.java
+++ src/main/java/homework9/MyArrayList.java
@@ -1,6 +1,7 @@
 package homework9;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class MyArrayList<E> {
     private static final int DEFAULT_CAPACITY = 10;
@@ -16,39 +17,38 @@ public class MyArrayList<E> {
 
     // Constructs an empty list with the specified initial capacity.
     public MyArrayList(int initialCapacity) {
-        if (initialCapacity > 0) {
-            this.elementData = new Object[initialCapacity]; // new object creation
-        } else if (initialCapacity == 0) {
-            this.elementData = EMPTY_ELEMENTDATA;
-        } else {
+
+        if (initialCapacity < 0){
             throw new IllegalArgumentException("Illegal Capacity: "+
                     initialCapacity);
         }
+
+        if (initialCapacity == 0) {
+            this.elementData = EMPTY_ELEMENTDATA;
+        }
+
+        if (initialCapacity > 0) {
+            this.elementData = new Object[initialCapacity]; // new object creation
+        }
     }
 
-    // Appends the specified element to the end of this list.
     public boolean add(E e) {
         ensureCapacity(size + 1);
         elementData[size++] = e;
         return true;
     }
 
-    // Increases the capacity of this ArrayList instance,
-    // if necessary, to ensure that it can hold at least the number
-    // of elements specified by the minimum capacity argument.
     private void ensureCapacity(int minCapacity)
     {
         if (elementData == DEFAULTCAPACITY_EMPTY_ELEMENTDATA) {
             minCapacity = Math.max(DEFAULT_CAPACITY, minCapacity);
         }
 
-        // overflow-conscious code
         if (minCapacity - elementData.length > 0) {
             grow(minCapacity);
         }
     }
 
-    // Expands the size of the list.
     private void grow(int minCapacity)
     {
         int oldCapacity = elementData.length;
@@ -58,12 +58,8 @@ public class MyArrayList<E> {
         elementData = Arrays.copyOf(elementData, newCapacity);
     }
 
-    // Removes the element at the specified position in this list.
-    // Shifts any subsequent elements to the left (subtracts one from their indices).
     public E remove(int index){
-        if (index < 0 | index >= size) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
-        }
+        Objects.checkIndex(index, size);
         int numMoved = size - index - 1;
         E elementToRemove = (E) elementData[index];
         System.arraycopy(elementData, index + 1, elementData, index, numMoved);
@@ -71,24 +67,17 @@ public class MyArrayList<E> {
         return elementToRemove;
     }
 
-    // Returns the number of elements in this list.
     public int size() {
         return size;
     }
 
-    // Returns the element at the specified position in this list.
     public E get(int index){
-        if (index >= size || index < 0){
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
-        }
+        Objects.checkIndex(index, size);
         return (E) elementData[index];
     }
 
-    // Removes all the elements from this list.
     public void clear() {
-        for (int i = 0; i < size; i++) {
-            elementData[i] = null;
-        }
+        elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
         size = 0;
     }
 
